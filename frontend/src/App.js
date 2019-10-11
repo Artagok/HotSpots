fc3b1fee8740de88dc5dd18bb4dfdeb4608611a1
Pau Núñez <linkinpau.97@gmail.com>
2019-10-12 01:06:45 +0200

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HotSpots</h1>
+      <h3>Under Construction!</h3>
+    </div>
+  );
+}
+
+export default App;
