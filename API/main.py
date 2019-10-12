f4d421f72a0398eeec077cb78cae0a44a41147b5
Ignasi Oliver <ignasioliverf@gmail.com>
2019-10-13 00:49:48 +0200

    Update main: implement CORS

--- API/main.py
+++ API/main.py
@@ -1,7 +1,7 @@
 import json
 from enum import Enum
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 
 class ModelName(str, Enum):
     kidnapping = "kidnapping"
@@ -25,6 +25,23 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http:localhost",
+    "http:localhost:8080",
+    "http:localhost:8000",
+    "http:localhost:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/type/{model_name}")
 async def type_specific(model_name: ModelName):
