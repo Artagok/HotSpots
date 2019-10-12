8219f11293fbbf5f3a128b3ad3b0b661c076de14
Alejandro Alarcon <alejandro@client208-10-192-69.eduroam.upc.edu>
2019-10-12 15:52:29 +0200

    afegir categories al desplegable

--- frontend/src/Components/Button.js
+++ frontend/src/Components/Button.js
@@ -22,6 +22,36 @@ const Button = (props) => {
         <DropdownItem>Robatori</DropdownItem>
         <DropdownItem divider />
         <DropdownItem>Accident de trànsit</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Voluntary manslaughter</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Burglary</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Rape</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Homicide</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Theft</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Shoplifting</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Vandalism</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Sexual assault</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Drug trafficking</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Fraud</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Public intoxication</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Disturbing the peace</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Extortion</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Justificable homicide</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Drug possession</DropdownItem>
       </DropdownMenu>
     </ButtonDropdown>
   );
