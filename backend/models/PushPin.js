b8976e5d47f1ea9191068d2038ee0d199616b56f
Pau Núñez <linkinpau.97@gmail.com>
2019-10-13 07:48:55 +0200

    Add locale, q_air and child_infra to Mongo

--- backend/models/PushPin.js
+++ backend/models/PushPin.js
@@ -6,9 +6,11 @@ const PushPinSchema = new Schema ({
   type: { type: String, required: true },
   description: { type: String },
   date: { type: Date, default: Date.now },
-  coords: {type: [Number], required: true },
-  event_count: {type: Number, required: true }
-  // img { type: Buffer }
+  coords: { type: [Number], required: true },
+  event_count: { type: Number, required: true },
+  locale: { type: String, required: true },
+  q_air: { type: String, required: true },
+  child_infra: { type: String, required: true },
 });
 
 // Export schema
