b0860850ce82bdc33a4f189e045e236b9e111d34
Ignasi Oliver <ignasioliverf@gmail.com>
2019-10-13 06:48:59 +0200

    Clean API trash

--- API/main.py
+++ API/main.py
@@ -1,8 +1,10 @@
 import json
 from enum import Enum
 from fastapi import FastAPI
-#from starlette.middleware.cors import CORSMiddleware
-#from pymongo import MongoClient
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from fastapi.encoders import jsonable_encoder
 
 class ModelName(str, Enum):
     kidnapping = "kidnapping"
@@ -23,12 +25,15 @@ class ModelName(str, Enum):
     justifiableHomicide = "justifiable-homicide"
     drugPossession = "drug-possession"
 
+class Pin(BaseModel):
+    type: str
+    description: str
+    coords: list
+    event_count: int
 
 app = FastAPI()
-"""
-client = MongoClient("mongodb+srv://admin:admin@hackupc2019-n6sxc.mongodb.net/HackUPC2019?retryWrites=true&w=majority")
-db = client.get_database('HackUPC2019')
-"""
+
+
 # Important to allow e.g. Express to run in order to connect with MongoDB
 origins = [
     "http:localhost",
@@ -37,17 +42,20 @@ origins = [
     "http:localhost:8000",
     "http://localhost:8000"
     "http:localhost:3000",
-    "http://localhost:3000",
+    "http://localhost:3000"
+    "http://localhost:5000",
+    "https://localhost:5000",
+    "147.83.201.96",
 ]
-"""
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], # allow_origin = origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-"""
+
 @app.get("/type/{model_name}")
 async def type_specific(model_name: ModelName):
     with open('whole_events.json') as json_file:
@@ -59,29 +67,26 @@ async def type_specific(model_name: ModelName):
     return to_return
 
 
+@app.get("/typeold")
+async def type_old():
+    with open('whole_events.json') as json_file:
+        data = json.load(json_file)
+    return data
+
+
 @app.get("/type")
-async def type_all():
-    with open('whole_events.json') as json_file:
-        data = json.load(json_file)
-    return data
-"""
-@app.post("/add/{type}/{description}/{cords}/{event_count}")
-async def addNewRecord(type: str, description: str, cords: list, event_count: int):
-    # db created globally, so always available - BAD practice, just to test:
+async def type_bd():
+    client = MongoClient(
+        "mongodb+srv://admin:admin@hackupc2019-n6sxc.mongodb.net/HackUPC2019?retryWrites=true&w=majority")
+    db = client.get_database('HackUPC2019')
     records = db.pushpins
-    new_pin = ({
-        "coords": cords,
-        "type": type,
-        "description": description,
-        "event_count": event_count,
-    })
-    records.insert_one(new_pin)
-    with open('whole_events.json') as json_file:
-        data = json.load(json_file)
-    return data
-"""
-"""
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.price, "item_id": item_id}
-"""
+    new_records = []
+    for element in list(records.find()):
+        new_subelement = {}
+        new_subelement["type"] = element["type"]
+        new_subelement["description"] = element["description"]
+        new_subelement["event_count"] = element["event_count"]
+        new_subelement["coords"] = element["coords"]
+        new_records.append(new_subelement)
+
+    return new_records
